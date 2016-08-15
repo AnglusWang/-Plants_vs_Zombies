6602566151bedde35bb2b017ba31c8ac6e6d51a7
AnglusWang <AnglusWang@163.com>
2016-08-15 15:56:50 +0800

    update

---
+++ app/src/main/java/com/angluswang/plants_vs_zombies/MainActivity.java
@@ -0,0 +1,13 @@
+package com.angluswang.plants_vs_zombies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
